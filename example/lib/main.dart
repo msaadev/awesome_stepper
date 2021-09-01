205450b5034edd91acbf158692378f1a6f72dfb2
samil demir <studi.msa@gmail.com>
2021-09-01 23:00:19 +0300

    editin toooo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,22 +1,79 @@
 import 'package:flutter/material.dart';
- 
+import 'package:lib_msaadev/lib_msaadev.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Stepper());
+  }
+}
+
+class Stepper extends StatelessWidget {
+  const Stepper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            decoration:
+                BoxDecoration(color: Colors.red, borderRadius: 5.customRadius),
+            margin: 5.paddingAll,
+            height: context.height * 0.1,
           ),
-        ),
+          Flexible(
+            child: PageView(
+              children: [
+                Container(
+                  margin: 5.paddingSymmetricHorizontal,
+                  decoration: BoxDecoration(
+                      color: Colors.green, borderRadius: 5.customRadius),
+                ),
+                Container(
+                  margin: 5.paddingSymmetricHorizontal,
+                  decoration: BoxDecoration(
+                      color: Colors.green, borderRadius: 5.customRadius),
+                ),
+                Container(
+                  margin: 5.paddingSymmetricHorizontal,
+                  decoration: BoxDecoration(
+                      color: Colors.green, borderRadius: 5.customRadius),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: 5.paddingAll,
+            height: context.height * 0.1,
+            decoration:
+                BoxDecoration(color: Colors.red, borderRadius: 5.customRadius),
+          ),
+        ],
       ),
     );
   }
 }
+
+
+/*
+Container(
+              margin: 5.paddingSymmetricHorizontal,
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: 5.customRadius),
+            )
+*/
