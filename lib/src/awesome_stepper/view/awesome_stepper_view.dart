60afbf5329bcfb2123c633be8e097a308e3b6075
samil demir <studi.msa@gmail.com>
2021-10-08 07:55:26 +0300

    performance updates

--- lib/src/awesome_stepper/view/awesome_stepper_view.dart
+++ lib/src/awesome_stepper/view/awesome_stepper_view.dart
@@ -2,7 +2,7 @@ import 'package:awesome_stepper/awesome_stepper.dart';
 import 'package:awesome_stepper/src/awesome_stepper/view_model/awesome_stepper_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:lib_msaadev/lib_msaadev.dart';
+import 'package:kartal/kartal.dart';
 
 class AwesomeStepper extends StatefulWidget {
   final List<AwesomeStepperItem> steps;
@@ -17,25 +17,26 @@ class AwesomeStepper extends StatefulWidget {
 class _AwesomeStepperState extends State<AwesomeStepper>
     with TickerProviderStateMixin {
   late final AwesomeStepperViewModel _viewModel;
-  late final Animation<Offset> _offsetAnimation;
+  late final Animation<double> textAnimation;
   late final AnimationController _controller, _circleValue;
   late final double ratio;
 
   @override
   void initState() {
     super.initState();
+
     _viewModel = AwesomeStepperViewModel();
     _controller = AnimationController(
       duration: const Duration(milliseconds: 250),
       vsync: this,
     );
     _circleValue = AnimationController(
-      duration: 1.secondDuration,
+      duration: Duration(seconds: 1),
       vsync: this,
     );
-    _offsetAnimation = Tween<Offset>(
-      begin: Offset.zero,
-      end: const Offset(0.0, -1.5),
+    textAnimation = Tween(
+      begin: 1.0,
+      end: 0.0,
     ).animate(CurvedAnimation(
       parent: _controller,
       curve: Curves.ease,
@@ -46,13 +47,14 @@ class _AwesomeStepperState extends State<AwesomeStepper>
 
     ratio = 1 / widget.steps.length;
 
-    Future.delayed(500.millisecondsDuration)
+    Future.delayed(Duration(milliseconds: 500))
         .whenComplete(() => _circleValue.animateTo(ratio));
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _circleValue.dispose();
     super.dispose();
   }
 
@@ -69,14 +71,15 @@ class _AwesomeStepperState extends State<AwesomeStepper>
 
   Container buildTop() {
     return Container(
-      height: context.customHeight(10),
+      height: context.dynamicHeight(.1),
+      
       color: Colors.grey,
       child: Row(
         children: [
           Container(
-            height: context.customHeight(10),
-            width: context.customHeight(10),
-            padding: 10.paddingAll,
+            height: context.dynamicHeight(.1),
+            width: context.dynamicHeight(.1),
+            padding: EdgeInsets.all(10),
             child: Observer(builder: (_) {
               return Stack(
                 fit: StackFit.expand,
@@ -92,8 +95,8 @@ class _AwesomeStepperState extends State<AwesomeStepper>
             }),
           ),
           Expanded(child: Observer(builder: (_) {
-            return SlideTransition(
-                position: _offsetAnimation,
+            return FadeTransition(
+                opacity: textAnimation,
                 child: Center(
                     child: Text(widget.steps[_viewModel.currentStep].label)));
           })),
@@ -110,17 +113,25 @@ class _AwesomeStepperState extends State<AwesomeStepper>
     return Row(
       children: [
         Expanded(
-          child: Container(
-            height: context.customHeight(9),
-            color: Colors.red,
-          ).onTap(() => tap(false)),
+          child: InkWell(
+            onTap:() => tap(false),
+            child: Container(
+              height: context.dynamicHeight(.1),
+              color: Colors.red,
+            ),
+          ),
         ),
-        10.wSized,
+        SizedBox(
+          width: 10,
+        ),
         Expanded(
-          child: Container(
-            height: context.customHeight(9),
-            color: Colors.green,
-          ).onTap(() => tap(true)),
+          child: InkWell(
+            onTap:() => tap(true),
+            child: Container(
+              height: context.dynamicHeight(.1),
+              color: Colors.green,
+            ),
+          ),
         ),
       ],
     );
@@ -134,7 +145,7 @@ class _AwesomeStepperState extends State<AwesomeStepper>
       isIncrement ? _viewModel.incrementStep() : _viewModel.decrementStep();
 
       if (widget.onStepChanged != null)
-        widget.onStepChanged!(_viewModel.currentStep + 1 );
+        widget.onStepChanged!(_viewModel.currentStep + 1);
 
       _controller.reverse();
     });
